737dda8bf852fec1a3a4a97e61adf396b8cc88f8
Magdiel Rotta Almeida <rotta.aero@gmail.com>
2023-12-18 12:45:17 -0300

    feat: entendendo encapsulamento de lógica e comunicação entre componentes

--- 01tut/src/App.js
+++ 01tut/src/App.js
@@ -1,47 +1,17 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
+import Header from './Header';
+import Content from './Content';
+import Footer from './Footer';
+
 
 function App() {
-  const handleNameChange = ()=>{
-    const names = ['Bom', 'Kevin', 'Mag'];
-    const int = Math.floor(Math.random()*3);
-    return names[int];
-  }
-
-  const name = "Maguid";
-
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello {handleNameChange()}!
-        </p>
-      <p>{name}</p>      
-      </header>
+      <Header/>
+      <Content/>
+      <Footer/>
     </div>
   );
 }
-
-// function App() {
-//   const handleNameChange = ()=>{
-//     const names = ['Bom', 'Kevin', 'Mag'];
-//     const int = Math.floor(Math.random()*3);
-//     return names[int];
-//   }
-
-//   const name = "Maguid";
-
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Hello {handleNameChange()}!
-//         </p>
-//       <p>{name}</p>      
-//       </header>
-//     </div>
-//   );
-// }
 export default App;
