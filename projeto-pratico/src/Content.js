685b69ee5aaed5970b660440318045746b4c5622
Magdiel Rotta Almeida <rotta.aero@gmail.com>
2023-12-18 17:51:56 -0300

    feat: 5 react click events

--- projeto-pratico/src/Content.js
+++ projeto-pratico/src/Content.js
@@ -4,11 +4,27 @@ const Content = () => {
         const int = Math.floor(Math.random()*3);
         return names[int];
     }
+
+    const handleClick= () => {
+        console.log('You clicked it')
+    }
+
+    const handleClick2 = (name) => {
+        console.log(`${name} was clicked`)
+    }
+
+    const handleClick3 = (e) => {
+        console.log(e.target.innerText)
+    }
+
     return (
     <main>
-        <p>
+        <p onDoubleClick={handleClick}>
             Hello, {handleNameChange()}!
         </p>
+        <button onClick={handleClick}>Click it</button>
+        <button onClick={()=> {handleClick2('Snoop')}}>Click it 2</button>
+        <button onClick={(e)=> handleClick3(e)}>Click it 3</button>
     </main>
     )
 }
