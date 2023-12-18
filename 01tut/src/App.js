dbd8ed972621a0cb9d9c9a2d99817f5720b68285
Magdiel Rotta Almeida <rotta.aero@gmail.com>
2023-12-17 22:13:33 -0300

    feat: criação do projeto

---
+++ 01tut/src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://www.github.com/Magdiel520"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Acess repository by clicking here
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
